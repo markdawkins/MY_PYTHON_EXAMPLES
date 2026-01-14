b07d2ebbabcd1a9782217bf3af9b582b2e4a07e2
markdawkins <44737733+markdawkins@users.noreply.github.com>
2026-01-14 16:22:54 -0500

    Update DNAC_Query.py

--- DNAC_Query.py
+++ DNAC_Query.py
@@ -7,7 +7,7 @@ import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 dnac = {
-    "host": "10.14.16.5",
+    "host": "10.x.x.x",
     "username": "YOUR_USERNAME",
     "password": "YOUR_PASSWORD",
     "version": "v1"
